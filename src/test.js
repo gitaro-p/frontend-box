6d8c4c3ef4467005a4a78a1fdf805e96feb1fc11
清川　裕太郎 <yuu7353@gmail.com>
2021-01-06 22:53:18 +0900

    eslint rule

---
+++ src/test.js
@@ -0,0 +1,13 @@
+//エラーになる
+
+/*function hello(name) {
+  document.body.textContent = 'Hello, ' + nama + '!';
+}
+
+hello('World');*/
+
+function hello(name) {
+  document.body.textContent = 'Hello, ' + name + '!';
+}
+
+hello('World');
