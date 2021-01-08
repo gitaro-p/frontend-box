6342ab1dc700209184c191f040d4da1ee098b84b
清川　裕太郎 <yuu7353@gmail.com>
2021-01-08 23:50:39 +0900

    i use eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,15 @@ module.exports = {
     project: './tsconfig.json' // tsconfig.jsonの場所を指定
   },
   plugins: [
-    '@typescript-eslint' // ESLintのTypeScriptプラグインのルールを適用できる様にする（/eslint-pluginは省略可）
+    '@typescript-eslint', // ESLintのTypeScriptプラグインのルールを適用できる様にする（/eslint-pluginは省略可）
+    'prettier'
   ],
   extends: [
     'eslint:recommended', // ESLintのJavaScriptルールセットを適用
     'plugin:@typescript-eslint/eslint-recommended', // eslint:recommendedに含まれるルールを型チェックでカバーできるものは無効化
     'plugin:@typescript-eslint/recommended', // 型チェックが不要なルールを適用
     'plugin:@typescript-eslint/recommended-requiring-type-checking', // 型チェックが必要なルールを適用
+    'prettier/@typescript-eslint', // Prettierでカバーできるルールを無効化
+    'plugin:prettier/recommended' // Prettierのお勧めルールセットを適用
   ]
-}
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true, // singleQuoteを有効
+  semi: false, // セミコロン禁止
+  'no-extra-semi': 'error', // 余分なセミコロン禁止
+  'no-unexpected-multiline': 'error', // 意図に反して2つの文が接続された箇所を警告
+}
